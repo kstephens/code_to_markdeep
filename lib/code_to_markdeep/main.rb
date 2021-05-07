bb9bac291508e91aa516552451264bc0a3110982
Kurt Stephens <git@kurtstephens.com>
2021-05-06 20:39:57 -0500

    Refactor :art state.

--- lib/code_to_markdeep/main.rb
+++ lib/code_to_markdeep/main.rb
@@ -397,6 +397,7 @@ module CodeToMarkdeep
     end
   end
 
+  # Start state:
   def start
     lang = line.lang
     if lang.name == :Markdown
@@ -479,31 +480,36 @@ module CodeToMarkdeep
 
   def art
     @art_lines ||= [ ]
-    case line = peek
+    case line = take
     when line.lang.art_rx
-      take
-      # Prepend '* ' to each line, if missing.
-      @art_lines.map! do | line |
-        line =~ /^\* / ? line : '* ' + line
-      end
-      # Calc with of fence
-      width = @art_lines.map(&:size).max + 2
-      art_border = '*' * width
-
-      out.puts art_border
-      str = String.new(line.to_s)
-      @art_lines.each do | line |
-        # Expand line to fence width
-        str << ' ' while str.size < width
-        str[width - 1] = '*'
-        out.puts str
-      end
-      out.puts art_border
+      emit_art! @art_lines
       @art_lines = nil
       state :md
     else
-      @art_lines << take
+      @art_lines << line
     end
+  end
+
+  def emit_art! lines
+    # Prepend '* ' to each line, if missing.
+    lines.map! do | line |
+      line =~ /^\* / ? line : '* ' + line
+    end
+
+    # Calc width of fence:
+    width = lines.map(&:size).max + 2
+    art_border = '*' * width
+
+    # Add fence:
+    lines.unshift art_border
+    lines.push    art_border
+
+    # Expand lines to fence width:
+    lines.map! do | line |
+      "%-#{width}s*" % [line.to_s]
+    end
+
+    lines.each{|l| out.puts l}
   end
 
   def md_code_fence
